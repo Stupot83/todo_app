72c5353a59e4147f2831f58556271cbc93e2bdc1
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-10-14 10:59:43 +0100

    :construction: Add failing test for Todo class instantiation

---
+++ spec/todo_spec.rb
@@ -0,0 +1,11 @@
+require "todo"
+
+describe "Todo" do
+  before :each do
+    @todo = Todo.new
+  end
+
+  it "should create an instance of the Todo class" do
+    expect(@todo).to be_instance_of(Todo)
+  end
+end
