177d1e809c1e0afc36275fe36feee81f21277406
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-10-14 11:04:16 +0100

    :construction: Add failing test for initialize with a string

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -2,10 +2,14 @@ require "todo"
 
 describe "Todo" do
   before :each do
-    @todo = Todo.new
+    @todo = Todo.new("Testing Todo")
   end
 
   it "should create an instance of the Todo class" do
     expect(@todo).to be_instance_of(Todo)
+  end
+
+  it "should contain a string with the instruction" do
+    expect(@todo.string).to eq("Testing Todo")
   end
 end
